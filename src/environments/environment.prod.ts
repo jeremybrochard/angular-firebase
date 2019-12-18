9861872d8c0d88a514a4cb8b6487da68e744e6aa
Jeremy BROCHARD <jrmy.brochard@gmail.com>
2019-12-19 00:55:38 +0100

    fix: complete prod env. config file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+import { FIREBASE_CONFIG } from './firebase-config';
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: FIREBASE_CONFIG.apiKey,
+    authDomain: FIREBASE_CONFIG.authDomain,
+    databaseURL: FIREBASE_CONFIG.databaseURL,
+    projectId: FIREBASE_CONFIG.projectId,
+    storageBucket: FIREBASE_CONFIG.storageBucket,
+    messagingSenderId: FIREBASE_CONFIG.messagingSenderId
+  }
 };
