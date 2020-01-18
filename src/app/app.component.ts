54c7e633052605383cb28b937c13081405e7ca9a
Jeremy BROCHARD <jrmy.brochard@gmail.com>
2020-01-18 18:33:08 +0100

    feat: implement ngx-translate and update signin and register pages to handle the new translation service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NOTIFICATIONS_OPTIONS } from './shared/notifications-options';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +12,17 @@ export class AppComponent implements OnInit {
   title = 'app';
   notificationOptions: any;
 
+  constructor(translate: TranslateService) {
+    this.initDefaultLanguage(translate);
+  }
+
   ngOnInit() {
     this.notificationOptions = NOTIFICATIONS_OPTIONS;
+  }
+
+  private initDefaultLanguage(translate: TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use('en');
   }
 
 }
